2b75e32702bfdef960ef1934bbeff3dcc0c4e31b
jadavv <jadavvinesh23@gmail.com>
2025-03-31 11:21:48 +0530

    31_03_2025

---
+++ basicpython.py
@@ -0,0 +1,80 @@
+
+# print(" start doing what you want to do in the fature ")
+
+
+
+# print("vs code used to extension list  ")
+# print(" python extension ")
+# print(" code runner ")
+# print(" live prieview ")
+# print(" live server ")
+
+
+
+#   /  ->  f  \ b
+
+# print("\"Hello World!\"")
+# print('"""Hello World!"""')
+# print("\"\"\"Hello World!\"\"\"")
+
+# id() - used to verify the identity of any variable
+# type() - used to identify the data type (return classes)
+
+
+# a = 45.69  
+# b = "str1"
+# print(id(a))
+# print(id(b))
+# print(type(a))
+# print(type(b))
+
+
+# a = 12      # int
+# a = 12.6    # float
+# a = 1-12j   # complex
+# a = "str"   # str
+# a = True    # bool (True/False)
+# a = b'\x01' # bytes
+# a = None
+a = 12
+
+# print(id(a), '-> ', type(a), '-> ', a)
+
+
+
+# name ="good morning"
+# age =22
+# price= 50.01
+# a=1-12j
+# b =True
+
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# print(type(a))
+# print(type(b))
+
+# a= 3
+# b= 5
+
+# print(a-b)
+
+
+# price1=float(input("enter the value _1 :"))
+# price2=float(input("enter the value _2 :"))
+
+# total=price1+price2
+# print(total)
+
+
+    #  name   
+    # age 
+    
+    # phone price
+    
+    # which phone 
+    
+    # your are going to home 
+    
+    
