38252c403c71e13806cd2d2a9216065b48441c9d
jadavv <jadavvinesh23@gmail.com>
2025-04-03 11:27:48 +0530

    3 apr

--- operators.py
+++ operators.py
@@ -13,53 +13,82 @@
 # Identity operators -> is, is not
 # Membership operators -> in, not in
 # Bitwise operators
-"""
-Arithmetic Operators
-python
-Copy
-Edit
-print(2 + 3)    # Addition: 5
-print(10 - 4)   # Subtraction: 6
-print(5 * 3)    # Multiplication: 15
-print(10 / 2)   # Division: 5.0
-print(10 % 3)   # Modulus: 1
-print(2 ** 3)   # Exponentiation: 8
-print(16 // 3)  # Floor Division: 5
-=================================================================
-Assignment Operators
-python
-Copy
-Edit
-x = 5  # Assign value
-x += 3 # Equivalent to x = x + 3 (x becomes 8)
-x -= 2 # Equivalent to x = x - 2 (x becomes 6)
-x *= 2 # Equivalent to x = x * 2 (x becomes 12)
-x /= 3 # Equivalent to x = x / 3 (x becomes 4.0)
-x %= 3 # Equivalent to x = x % 3 (x becomes 1.0)
-========================================================================
-Comparison Operators
-python
-Copy
-Edit
-a = 10
-b = 20
-print(a == b)  # False
-print(a != b)  # True
-print(a > b)   # False
-print(a < b)   # True
-print(a >= 10) # True
-print(b <= 15) # False
-===============================================================================
-Logical Operators
-python
-Copy
-Edit
-x = True
+
+# Arithmetic Operators
+# python
+# Copy
+# Edit
+# print(3 + 3)    # Addition: 6
+# print(15 - 4)   # Subtraction: 11
+# print(6 * 3)    # Multiplication: 18
+# print(10 / 5)   # Division: 2.0
+# print(100 % 2)   # Modulus: 0
+# print(2 ** 2)   # Exponentiation: 4  2*2*2*2*2*2*2*2* = 256
+# print(12 // 2)  # Floor Division:6 
+# =================================================================
+
+# Assignment Operators
+# python
+
+# multi  line code  shurtcut  ctrl +alt
+# x = 5  # Assign value
+# x += 3 # Equivalent to x = x + 3 (x becomes 8)
+# print(x)
+
+# x = 5  # Assign value
+
+# x -= 2 # Equivalent to x = x - 2 (x becomes 6)
+# print(x)
+# x = 5  # Assign value
+
+# x *= 2 # Equivalent to x = x * 2 (x becomes 12)
+# print(x)
+
+# x /= 3 # Equivalent to x = x / 3 (x becomes 4.0)
+# print(x)
+# x = 15  # Assign value
+
+# x %= 7 # Equivalent to x = x % 3 (x becomes 1.0)  
+# print(x)
+
+# ========================================================================
+# Comparison Operators
+# python
+# Copy
+# Edit
+# a = 25
+# b = 41
+# print(a == b)  
+# print(a != b)  
+# print(a > b)   
+# print(a < b)   
+# print(a >= 10) 
+# print(b <= 15)
+
+# ===============================================================================
+# Logical Operators
+# python
+# Copy
+# Edit
+
+# AND (and) → Both must be true ✅✅ → True, otherwise False.     
+
+# Example: "I will go outside if it's sunny AND I have free time."
+
+# OR (or) → At least one must be true ✅❌ or ❌✅ or ✅✅ → True.    
+
+# Example: "I will eat ice cream if it's hot OR I feel like it."
+
+# NOT (not) → Reverses the truth.
+
+# Example: "If I am NOT tired, I will study."
+x = False
 y = False
-print(x and y) # False
+# print(x and y) # False 
 print(x or y)  # True
-print(not x)   # False
-========================================================================================
+# print(not x)   # False
+
+"""========================================================================================
 Identity Operators
 python
 Copy
---
+++ try.py
@@ -0,0 +1,17 @@
+# a,b=1,1.2
+# sum=a+b
+# print(sum)
+
+
+a,b=1,"2"
+
+sum=a+b
+print(sum)
+
+
+
+
+# a,b=1,"2"
+# c=int(b)
+# sum=a+c
+# print(sum)
