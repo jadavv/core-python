9711a0d5cf8dca24094a17cb4d0f11662bc0a6c5
jadavv <jadavvinesh23@gmail.com>
2025-04-07 11:53:10 +0530

    7 apr

--- operators.py
+++ operators.py
@@ -30,10 +30,10 @@
 # Assignment Operators
 # python
 
-# multi  line code  shurtcut  ctrl +alt
+# multi  line code  shurtcut  ctrl +alt 
 # x = 5  # Assign value
 # x += 3 # Equivalent to x = x + 3 (x becomes 8)
-# print(x)
+# print(x) 
 
 # x = 5  # Assign value
 
@@ -82,44 +82,69 @@
 # NOT (not) → Reverses the truth.
 
 # Example: "If I am NOT tired, I will study."
-x = False
-y = False
+# x = False
+# y = False
 # print(x and y) # False 
-print(x or y)  # True
+# print(x or y)  # True
 # print(not x)   # False
 
-"""========================================================================================
-Identity Operators
-python
-Copy
-Edit
-a = [1, 2, 3]
-b = [1, 2, 3]
-c = a
-print(a is c)    # True (same object in memory)
-print(a is b)    # False (different objects, even with same values)
-print(a is not b) # True
-==========================================================================================
-Membership Operators
-python
-Copy
-Edit
-numbers = [1, 2, 3, 4, 5]
-print(3 in numbers)    # True
-print(6 not in numbers) # True
-==============================================================================================
-Bitwise Operators
-python
-Copy
-Edit
-a = 5  # 0101 in binary
-b = 3  # 0011 in binary
+# ========================================================================================
+# Identity Operators
+# python
+# Copy
+# Edit
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# c = a
+# print(a is c)    # True (same object in memory)
+# print(a is b)    # False (different objects, even with same values)
+# print(a is not b) # True
+# print(a is  not c)
+# ==========================================================================================
+# Membership Operators
+# python
+# Copy
+# Edit
+# numbers = [14, 42, 37, 74, 75,"pratik","ashish","ritik"]
+# # print(3 in numbers)    # True
+# # print(1 not in numbers) # True
+# print("pratik" in numbers) # true
+# print(42 in numbers) # true
+# print("ashish" not in numbers) # false 
+# ==============================================================================================
+# Bitwise Operators
+# python
+# Copy
+# Edit
+a = 6  # 0110 in binary       2   7   5
+b = 3  # 0011 in binary      
 
-print(a & b)  # 1 (0001) - AND
+print(a & b)  # 2 (0001) - AND
 print(a | b)  # 7 (0111) - OR
-print(a ^ b)  # 6 (0110) - XOR
-print(~a)     # -6 (inverts bits)
-print(a << 1) # 10 (shifts left by 1)
-print(a >> 1) # 2 (shifts right by 1)
-======================================================================================================
-"""
+print(a ^ b)  # 5 (0110) - XOR
+
+
+# print(~a)     # -6 (inverts bits)
+# print(a << 1) # 10 (shifts left by 1)
+# print(a >> 1) # 2 (shifts right by 1)
+# ======================================================================================================
+# Decimal, Hexadecimal, and Octal chart for numbers 1 to 16:
+
+# Decimal (Base 10)	Hexadecimal (Base 16)	Octal (Base 8)	Binary (Base 2)
+# 1		                                      1	             0001
+# 2	                                           2	             0010
+# 3	                                           3	             0011
+# 4	                                           4	             0100
+# 5	                                           5	             0101
+# 6	                                           6	             0110
+# 7	                                           7	             0111
+# 8	                                           10	             1000
+# 9	                                           11	             1001
+# 10	A	                                  12	            1010
+# 11	B	                                  13	            1011
+# 12	C	                                  14	            1100
+# 13	D	                                  15	            1101
+# 14	E	                                  16	            1110
+# 15	F	                                  17	            1111
+# 16	10	                                  20	            10000
+                                  
---
+++ pratical/arithmetical.py
@@ -0,0 +1,72 @@
+# # type conversion
+
+# a=10
+# b=23
+
+# print(a+b)
+
+
+# a,b= 3,2.0
+# print(a+b)
+
+
+# # type casting
+# a,b= 50,5
+
+# sum=str(b)
+# c=a+sum
+# print(c)
+
+
+# Let‘s Practice
+# Write a Program to input 2 numbers & print their sum
+
+# NAME=input("Enter the user name :")
+# iphone=input("Enter the model name :")
+# price=int(input("enter the price:"))
+# discount=int(input("best discount:"))
+# total=price-discount
+
+# print("user name:",NAME)
+# print("iphoone model name:",iphone)
+# print("iphone total price with discount",total)
+
+# WAP to input side of a square & print its area
+
+# side=float(input("enter the side of the square "))
+
+
+# area=side*side
+
+# print("the area of the square is:",area)
+
+
+# Let‘s Practice
+# WAP to input 2 int numbers, a and b.
+# Print True if a is greater than or equal to b. If not print False.
+
+# a=int(input("enter the first number :"))#45
+# b=int(input("enter the second number :"))#40
+
+# print(a<b)
+
+# print(a>b)
+# print(a<=b)
+# print(a>=b)
+
+
+# string 
+
+# basic oprations
+
+
+# concatanation
+
+a="ashish"
+b=" is good "
+c="boy"
+
+d=a+b
+e=d+c
+print(e)
+# print(a,b,c)
